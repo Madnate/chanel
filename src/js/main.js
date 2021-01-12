27aa9c497ad206c29ddceedbd85aa7df2b056ac9
Madnate <ca.nate104@gmail.com>
2021-01-12 21:40:20 +0100

    Transition + parallax

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
 const accordions = document.getElementsByClassName("schedule__action");
 
-
 for (let i = 0; i < accordions.length; i++) {
   accordions[i].addEventListener('click', function(e) {
     accordions[i].classList.toggle('active');
     this.nextElementSibling.classList.toggle('is-open');
   });
 }
+
+const rellax = new Rellax('.rellax', {
+  wrapper: null,
+  round: true,
+  vertical: true,
+});
